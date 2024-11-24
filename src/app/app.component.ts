d051ed0f282ac573d30764615c0c174fcabdcbdb
ravindrasharmacs <ravindra.sharmacs@gmail.com>
2024-11-24 16:24:40 +0530

    pr_003 : setup routes for home and about page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [FormsModule] // Include FormsModule in the imports array
+  imports: [CommonModule, RouterModule,FormsModule] // Include FormsModule in the imports array
 })
 export class AppComponent {
   title = 'AppComponent';
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { AppModule } from './app/app.module';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent);
+
+bootstrapApplication(AppComponent , {
+    providers: [
+        provideRouter(routes),
+    ]
+}).catch(err => console.error(err));
