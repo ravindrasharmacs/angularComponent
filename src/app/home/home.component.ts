c7f3e39f25fac3c64bea964c017772ae8207501d
ravindrasharmacs <ravindra.sharmacs@gmail.com>
2024-11-24 23:01:50 +0530

    pr_006 : add form validation for input fields

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DataService } from '../data.service';
+import { ReactiveFormsModule , FormBuilder , FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule , FormsModule],
+  imports: [CommonModule , FormsModule , ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   //Component code goes here
-  name : string = "";
+  nameOld : string = "";
   items : string[] = [];
+  registrationForm: FormGroup;
 
-  constructor(private dataService: DataService) {
+  constructor(private dataService: DataService, private fb: FormBuilder) {
     this.items = this.dataService.getItems();
+
+    this.registrationForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
 }
+  ngOnInit(): void {}
+  get name() {
+    return this.registrationForm.get('name');
+  }
+  get email() {
+    return this.registrationForm.get('email');
+  }
+  get password() {
+    return this.registrationForm.get('password');
+  }
+
+onSubmit() : void {
+  if(this.registrationForm.valid){
+    console.log('Form submitted', this.registrationForm.value);
+  } else {
+    console.log('Form invalid');
+  }
+}
+
 addItem(item: string) : void {
   if(item){
     this.dataService.addItem(item);
